644bf593e2ba134fc2d82b15dad651e49836c2fa
lata bansavade <lata@bmconsuting.co>
2024-12-28 14:24:37 +0530

    coding questions

---
+++ problem-1/first.js
@@ -0,0 +1,22 @@
+function countChar(str){
+    str  = str.replace(/\s+/g, '').toLowerCase();
+  
+    const countMap ={}
+  
+    for(let char of str){
+      countMap[char] = (countMap[char] || 0) + 1;
+  
+    }
+  
+    const result = []
+  
+    for (let char of str){
+      if(!result.includes(char)){
+          result.push(char)
+          console.log(`${char.toUpperCase()}-${countMap[char]}`);
+          
+      }
+    }
+  
+  }
+  countChar("Amolya Sharma")
